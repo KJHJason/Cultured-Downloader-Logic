f40ce18b965a1982f3ed854511f7322f536d41f6
KJHJason <kuanjunhaojason@gmail.com>
2024-05-02 21:24:39 +0800

    updated gdrive init code

--- gdrive/gdrive.go
+++ gdrive/gdrive.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"regexp"
 
+	"github.com/KJHJason/Cultured-Downloader-Logic/configs"
 	"github.com/KJHJason/Cultured-Downloader-Logic/errors"
 	"github.com/KJHJason/Cultured-Downloader-Logic/httpfuncs"
+	"github.com/KJHJason/Cultured-Downloader-Logic/iofuncs"
 	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
 )
 
 const (
@@ -22,13 +25,11 @@ const (
 )
 
 var (
-	API_KEY_REGEX       = regexp.MustCompile(fmt.Sprintf(`^%s$`, BASE_API_KEY_REGEX_STR))
+	API_KEY_REGEX = regexp.MustCompile(fmt.Sprintf(`^%s$`, BASE_API_KEY_REGEX_STR))
 	API_KEY_PARAM_REGEX = regexp.MustCompile(fmt.Sprintf(`key=%s`, BASE_API_KEY_REGEX_STR))
 )
 
 type GDrive struct {
-	Ctx                context.Context
-	cancel             context.CancelFunc
 	apiKey             string         // Google Drive API key to use
 	client             *drive.Service // Google Drive service client (if using service account credentials)
 	apiUrl             string         // https://www.googleapis.com/drive/v3/files
@@ -37,40 +38,55 @@ type GDrive struct {
 	maxDownloadWorkers int            // max concurrent workers for downloading files
 }
 
-func NewGDrive(ctx context.Context, timeout, dlTimeout, maxDlWorkers int) *GDrive {
-	ctx, cancel := context.WithCancel(ctx)
-	return &GDrive{
-		Ctx:                ctx,
-		cancel:             cancel,
-		apiUrl:             "https://www.googleapis.com/drive/v3/files",
-		timeout:            timeout,
-		downloadTimeout:    dlTimeout,
-		maxDownloadWorkers: maxDlWorkers,
+// Returns a GDrive structure with the given API key and max download workers
+func GetNewGDrive(apiKey, jsonPath string, config *configs.Config, maxDownloadWorkers int) (*GDrive, error) {
+	if jsonPath != "" && apiKey != "" {
+		return nil, fmt.Errorf(
+			"gdrive error %d: Both Google Drive API key and service account credentials file cannot be used at the same time",
+			errs.DEV_ERROR,
+		)
+	} else if jsonPath == "" && apiKey == "" {
+		return nil, fmt.Errorf(
+			"gdrive error %d: Google Drive API key or service account credentials file is required",
+			errs.DEV_ERROR,
+		)
 	}
-}
 
-func (gdrive *GDrive) SetContext(ctx context.Context) {
-	gdrive.Ctx, gdrive.cancel = context.WithCancel(ctx)
-}
+	gdrive := &GDrive{
+		apiUrl:             "https://www.googleapis.com/drive/v3/files",
+		timeout:            15,
+		downloadTimeout:    900, // 15 minutes
+		maxDownloadWorkers: maxDownloadWorkers,
+	}
+	if apiKey != "" {
+		gdrive.apiKey = apiKey
+		gdriveIsValid, err := gdrive.GDriveKeyIsValid(config.UserAgent)
+		if err != nil {
+			return nil, err
+		} else if !gdriveIsValid {
+			return nil, fmt.Errorf(
+				"gdrive error %d: Google Drive API key is invalid",
+				errs.INPUT_ERROR,
+			)
+		}
+		return gdrive, nil
+	} 
 
-func (gdrive *GDrive) SetApiKey(apiKey string) {
-	gdrive.apiKey = apiKey
-}
-
-func (gdrive *GDrive) SetClient(client *drive.Service) {
-	gdrive.client = client
-}
-
-func (gdrive *GDrive) SetTimeout(timeout int) {
-	gdrive.timeout = timeout
-}
-
-func (gdrive *GDrive) SetDownloadTimeout(downloadTimeout int) {
-	gdrive.downloadTimeout = downloadTimeout
-}
-
-func (gdrive *GDrive) SetMaxDownloadWorkers(maxDownloadWorkers int) {
-	gdrive.maxDownloadWorkers = maxDownloadWorkers
+	if !iofuncs.PathExists(jsonPath) {
+		return nil, fmt.Errorf(
+			"unable to access Drive API due to missing credentials file: %s",
+			jsonPath,
+		)
+	}
+	srv, err := drive.NewService(context.Background(), option.WithCredentialsFile(jsonPath))
+	if err != nil {
+		return nil, fmt.Errorf(
+			"unable to access Drive API due to %w",
+			err,
+		)
+	}
+	gdrive.client = srv
+	return gdrive, nil
 }
 
 // Checks if the given Google Drive API key is valid
@@ -96,7 +112,7 @@ func (gdrive *GDrive) GDriveKeyIsValid(userAgent string) (bool, error) {
 	)
 	if err != nil {
 		return false, fmt.Errorf(
-			"gdrive error %d: failed to check if Google Drive API key is valid, more info => %w",
+			"gdrive error %d: failed to check if Google Drive API key is valid, more info => %v",
 			errs.CONNECTION_ERROR,
 			err,
 		)
