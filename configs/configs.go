a6e7b9e159ef34bab5bf2015cdc13f7e3fb11442
KJHJason <kuanjunhaojason@gmail.com>
2024-05-08 12:43:41 +0800

    edit ffmpeg validation logic

--- configs/configs.go
+++ configs/configs.go
@@ -1,7 +1,11 @@
 package configs
 
 import (
+	"context"
+	"errors"
 	"os/exec"
+	"strings"
+	"time"
 )
 
 type Config struct {
@@ -24,7 +28,28 @@ type Config struct {
 	UserAgent      string
 }
 
-func (c *Config) ValidateFfmpegPathLogic() error {
-	_, ffmpegErr := exec.LookPath(c.FfmpegPath)
-	return ffmpegErr
+func ValidateFfmpegPathLogic(ctx context.Context, ffmpegPath string) error {
+	_, ffmpegErr := exec.LookPath(ffmpegPath)
+	if ffmpegErr != nil {
+		return ffmpegErr
+	}
+
+	cmdCtx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	// execute the ffmpeg binary to check if it's working
+	cmd := exec.CommandContext(cmdCtx, ffmpegPath, "-version")
+	stdout, ffmpegErr := cmd.Output()
+	if ffmpegErr != nil {
+		return ffmpegErr
+	}
+
+	if len(stdout) > 0 && strings.HasPrefix(string(stdout), "ffmpeg version") {
+		return nil
+	}
+	return errors.New("unexpected output from ffmpeg binary, please ensure it is the correct ffmpeg binary")
+}
+
+func (c *Config) ValidateFfmpegPathLogic(ctx context.Context) error {
+	return ValidateFfmpegPathLogic(ctx, c.FfmpegPath)
 }
