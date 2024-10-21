967f3ed3d5f9863e5b0a340e0cb3bf6e9e19eb24
KJHJason <git@kjhjason.com>
2024-10-22 00:54:44 +0800

    gofmt

--- utils/utils.go
+++ utils/utils.go
@@ -18,9 +18,9 @@ import (
 )
 
 type PrimitiveTypes interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 | 
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
-	~string 
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~string
 }
 
 func RemoveDuplicatesFromSlice[T PrimitiveTypes](s []T) []T {
